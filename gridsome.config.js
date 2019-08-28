d2af5272cb1569fe3d9ee4b7302bf46d205ba8d1
Steff Beckers <steff@steffbeckers.eu>
2019-08-28 21:23:39 +0200

    Moving folders

---
+++ gridsome.config.js
@@ -0,0 +1,39 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "Steff Beckers",
+  siteDescription:
+    "Thoughts, stories and ideas. I write about Web Development and DevOps.",
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: "@gridsome/source-filesystem",
+      options: {
+        typeName: "Post",
+        path: "content/posts/*.md",
+        route: "/:slug",
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: "Tag",
+            route: "/tag/:id",
+            create: true
+          }
+        }
+      }
+    }
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+};
