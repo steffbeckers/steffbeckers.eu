86724873b71beb9497f0b52f73bbc73c8a04a171
Steff Beckers <steff@steffbeckers.eu>
2021-01-27 23:14:29 +0100

    Nuxt config update for 404.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  // Generate options
+  generate: {
+    fallback: true,
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Steff Beckers',
