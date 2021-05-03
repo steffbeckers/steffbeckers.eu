2ff995e884d55ace397db55fb7d030c2ef69f130
Steff Beckers <steff@steffbeckers.eu>
2021-05-03 22:04:03 +0200

    Codefined update

--- pages/experience.vue
+++ pages/experience.vue
@@ -1,152 +1,203 @@
 <template>
   <div class="flex flex-col space-y-8">
-    <div class="flex flex-col space-y-4">
+    <div class="flex flex-col space-y-6">
       <div class="flex flex-col">
         <h1>Work experience<span class="underscore">_</span></h1>
         <div class="subtitle">Many lines of code and coffee</div>
       </div>
-      <div class="flex flex-col space-y-2">
-        <div class="flex flex-col">
-          <a href="https://www.aarixa.be" target="_blank">
-            <img
-              v-if="$colorMode.value === 'light'"
-              class="logo mb-2"
-              src="~/assets/images/logos/aariXa.png"
-              alt="Logo aariXa"
-            />
-            <img
-              v-if="$colorMode.value === 'dark'"
-              class="logo mb-2"
-              src="~/assets/images/logos/aariXa-white.png"
-              alt="Logo aariXa"
-            />
-          </a>
-          <h2>Analyst Developer</h2>
-          <div class="subtitle">July 2017 - Present</div>
-        </div>
+      <div class="flex flex-col space-y-4">
         <div class="flex flex-col space-y-2">
-          <p>
-            aariXa's development stack includes: Angular, .NET Core, MS SQL,
-            Azure, Windows Server, ... so mainly focused on Microsoft
-            technologies. I have been working on a new Cloud Native, realtime,
-            multi-tenant web app hosted as microservices which run in Docker
-            containers as an in-house developer.
-          </p>
-          <p>
-            I have migrated aariXa's existing Dynamics CRM 2011 to a new .NET
-            based CRM package and customized the CRM with new business logic.
-          </p>
-          <p>
-            As one of the few in-house developers, I've also been doing some
-            DevOps and system administration. Moving existing applications to
-            new containers or virtual machines, but also monitoring and
-            maintaining aariXa's infrastructure.
-          </p>
-        </div>
-        <div class="flex flex-col space-y-2">
-          <h3>Mainly used skills and technologies</h3>
-          <skills-list
-            :skills="[
-              {
-                name: '.NET',
-              },
-              {
-                name: 'C#',
-              },
-              {
-                name: 'Angular',
-              },
-              {
-                name: 'TypeScript',
-              },
-              {
-                name: 'SQL Server',
-              },
-              {
-                name: 'Docker',
-              },
-              {
-                name: 'Microservices',
-              },
-              {
-                name: 'Azure',
-              },
-              {
-                name: 'Windows Server',
-              },
-              {
-                name: 'Ubuntu Server',
-                icon: 'ubuntu.png',
-              },
-              {
-                name: 'Azure DevOps',
-              },
-            ]"
-          ></skills-list>
-        </div>
-      </div>
-      <hr />
-      <div class="flex flex-col space-y-2">
-        <div class="flex flex-col">
-          <div class="flex flex-row items-center space-x-4">
-            <a href="https://be.ctg.com" target="_blank">
-              <img
-                class="logo mb-2"
-                src="~/assets/images/logos/CTG.gif"
-                alt="Logo CTG"
-              />
-            </a>
-            <a href="https://www.bridgestone.eu" target="_blank">
+          <div class="flex flex-col">
+            <a href="https://codefined.be" target="_blank">
               <img
                 v-if="$colorMode.value === 'light'"
                 class="logo mb-2"
-                src="~/assets/images/logos/Bridgestone.png"
-                alt="Logo Bridgestone"
+                src="~/assets/images/logos/Codefined.svg"
+                alt="Logo Codefined"
               />
               <img
                 v-if="$colorMode.value === 'dark'"
                 class="logo mb-2"
-                src="~/assets/images/logos/Bridgestone-white.png"
-                alt="Logo Bridgestone"
+                src="~/assets/images/logos/Codefined-white.svg"
+                alt="Logo Codefined"
               />
             </a>
+            <h2>Software Developer</h2>
+            <div class="subtitle">May 2021 - Present</div>
           </div>
-          <h2>Consultant for CTG on behalf of Bridgestone Europe</h2>
-          <div class="subtitle">October 2016 - June 2017</div>
+          <!-- <div class="flex flex-col space-y-2">
+            <p></p>
+          </div> -->
+          <div class="flex flex-col space-y-2">
+            <h3>Mainly used skills and technologies</h3>
+            <skills-list
+              :skills="[
+                {
+                  name: '.NET',
+                },
+                {
+                  name: 'C#',
+                },
+                {
+                  name: 'ABP.io',
+                },
+                {
+                  name: 'SQL Server',
+                },
+                {
+                  name: 'Azure',
+                },
+                {
+                  name: 'Azure DevOps',
+                },
+              ]"
+            ></skills-list>
+          </div>
         </div>
+        <hr />
         <div class="flex flex-col space-y-2">
-          <p>
-            I have worked on a project for Bridgestone as CRM Developer,
-            customizing Dynamics 365 CRM with business logic. Like writing
-            plugins in C# and building custom UI components and apps with
-            TypeScript, Knockout.js and many more of the latest technologies.
-          </p>
+          <div class="flex flex-col">
+            <a href="https://www.aarixa.be" target="_blank">
+              <img
+                v-if="$colorMode.value === 'light'"
+                class="logo mb-2"
+                src="~/assets/images/logos/aariXa.png"
+                alt="Logo aariXa"
+              />
+              <img
+                v-if="$colorMode.value === 'dark'"
+                class="logo mb-2"
+                src="~/assets/images/logos/aariXa-white.png"
+                alt="Logo aariXa"
+              />
+            </a>
+            <h2>Analyst Developer</h2>
+            <div class="subtitle">July 2017 - April 2021</div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p>
+              aariXa's development stack includes: Angular, .NET Core, MS SQL,
+              Azure, Windows Server, ... so mainly focused on Microsoft
+              technologies. I have been working on a new Cloud Native, realtime,
+              multi-tenant web app hosted as microservices which run in Docker
+              containers as an in-house developer.
+            </p>
+            <p>
+              I have migrated aariXa's existing Dynamics CRM 2011 to a new .NET
+              based CRM package and customized the CRM with new business logic.
+            </p>
+            <p>
+              As one of the few in-house developers, I've also been doing some
+              DevOps and system administration. Moving existing applications to
+              new containers or virtual machines, but also monitoring and
+              maintaining aariXa's infrastructure.
+            </p>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <h3>Mainly used skills and technologies</h3>
+            <skills-list
+              :skills="[
+                {
+                  name: '.NET',
+                },
+                {
+                  name: 'C#',
+                },
+                {
+                  name: 'Angular',
+                },
+                {
+                  name: 'TypeScript',
+                },
+                {
+                  name: 'SQL Server',
+                },
+                {
+                  name: 'Docker',
+                },
+                {
+                  name: 'Microservices',
+                },
+                {
+                  name: 'Azure',
+                },
+                {
+                  name: 'Windows Server',
+                },
+                {
+                  name: 'Ubuntu Server',
+                  icon: 'ubuntu.png',
+                },
+                {
+                  name: 'Azure DevOps',
+                },
+              ]"
+            ></skills-list>
+          </div>
         </div>
+        <hr />
         <div class="flex flex-col space-y-2">
-          <h3>Mainly used skills and technologies</h3>
-          <SkillsList
-            :skills="[
-              {
-                name: 'Dynamics 365 CRM',
-              },
-              {
-                name: 'C#',
-              },
-              {
-                name: 'TypeScript',
-              },
-              {
-                name: 'HTML5',
-              },
-              {
-                name: 'CSS3',
-              },
-              {
-                name: 'SASS',
-                devicon: 'sass-original',
-              },
-            ]"
-          />
+          <div class="flex flex-col">
+            <div class="flex flex-row items-center space-x-4">
+              <a href="https://be.ctg.com" target="_blank">
+                <img
+                  class="logo mb-2"
+                  src="~/assets/images/logos/CTG.gif"
+                  alt="Logo CTG"
+                />
+              </a>
+              <a href="https://www.bridgestone.eu" target="_blank">
+                <img
+                  v-if="$colorMode.value === 'light'"
+                  class="logo mb-2"
+                  src="~/assets/images/logos/Bridgestone.png"
+                  alt="Logo Bridgestone"
+                />
+                <img
+                  v-if="$colorMode.value === 'dark'"
+                  class="logo mb-2"
+                  src="~/assets/images/logos/Bridgestone-white.png"
+                  alt="Logo Bridgestone"
+                />
+              </a>
+            </div>
+            <h2>Consultant for CTG on behalf of Bridgestone Europe</h2>
+            <div class="subtitle">October 2016 - June 2017</div>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <p>
+              I have worked on a project for Bridgestone as CRM Developer,
+              customizing Dynamics 365 CRM with business logic. Like writing
+              plugins in C# and building custom UI components and apps with
+              TypeScript, Knockout.js and many more of the latest technologies.
+            </p>
+          </div>
+          <div class="flex flex-col space-y-2">
+            <h3>Mainly used skills and technologies</h3>
+            <SkillsList
+              :skills="[
+                {
+                  name: 'Dynamics 365 CRM',
+                },
+                {
+                  name: 'C#',
+                },
+                {
+                  name: 'TypeScript',
+                },
+                {
+                  name: 'HTML5',
+                },
+                {
+                  name: 'CSS3',
+                },
+                {
+                  name: 'SASS',
+                  devicon: 'sass-original',
+                },
+              ]"
+            />
+          </div>
         </div>
       </div>
     </div>
