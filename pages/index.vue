ee65c4e21339489e4fd2886ad28042d3e678e6c9
Steff Beckers <steff@steffbeckers.eu>
2021-05-04 23:44:07 +0200

    CV update

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
           </p>
           <p>
             Feel free to get in touch or download my CV.<br />
-            Last updated on 05/03/2021.
+            Last updated on 04/05/2021.
           </p>
         </div>
         <div class="flex flex-row space-x-4 sm:flex-col sm:space-x-0">
