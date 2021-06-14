9877d87af3796d6ed7d0908e77ca6e3e59983908
Steff Beckers <steff@steffbeckers.eu>
2021-06-14 18:10:29 +0200

    Revert highlight.js dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,8 +47,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href:
-          'https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css',
+        href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css',
       },
     ],
   },
