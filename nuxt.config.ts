c5cbacfe8a0b8bfa6ede14c43389539c4bb83dfc
Steff Beckers <steff@steffbeckers.eu>
2023-06-16 19:22:50 +0200

    Color mode added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  colorMode: {
+    classSuffix: "",
+  },
   devtools: {
     enabled: true,
   },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
 });
