6b99dd3bbd424d23a3508516cb1761fb485d2d53
Steff Beckers <steff@steffbeckers.eu>
2023-06-16 22:51:47 +0200

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+      ],
+    },
+  },
   // https://color-mode.nuxtjs.org/#configuration
   colorMode: {
     classSuffix: "",
