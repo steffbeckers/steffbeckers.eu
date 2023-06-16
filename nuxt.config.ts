9cb25455c10c10e00b35e3b09f867ed71d38d3bc
Steff Beckers <steff@steffbeckers.eu>
2023-06-16 21:17:41 +0200

    Cookie control banner description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,15 @@ export default defineNuxtConfig({
   },
   // https://content.nuxtjs.org/api/configuration
   content: {},
+  // https://nuxt.com/modules/cookie-control#module-options
+  cookieControl: {
+    localeTexts: {
+      en: {
+        bannerDescription:
+          "I use my own and third-party cookies so that I can display this website correctly and better understand how this website is used, with a view to improving the services I offer. A decision on cookie usage permissions can be changed anytime using the cookie button that will appear after a selection has been made on this banner.",
+      },
+    },
+  },
   // https://devtools.nuxtjs.org/guide#module-options
   devtools: {
     enabled: true,
